fc23c38497613d80ef6e7e1fe2dd4af6be65b4e1
Nguyễn Đăng Định <57066661+nguyendangdinh47@users.noreply.github.com>
2022-01-29 18:44:20 +0700

    add upload image

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,3 +4,5 @@ export const scheduleController = require('./schedule.controller');
 export const timeSlotController = require('./timeSlot.controller');
 export const doctorController = require('./doctor.controller');
 export const bookingController = require('./booking.controller');
+export const positionController = require('./position.controller');
+export const specialistController = require('./specialist.controller');
--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ let app = express();
 app.use(cors({ origin: true }));
 
 app.use(express.static('public'));
-// app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
